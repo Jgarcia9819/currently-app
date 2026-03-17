d18ea82f594b69430846d0ec097db7a755527df2
Joshua Garcia <138169448+Jgarcia9819@users.noreply.github.com>
2026-03-17 11:47:13 -0700

    update

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Currently:";
-export const SITE_DESCRIPTION = "A State of Mind Journal";
+export const SITE_TITLE = "Currently: A State of Mind Journal";
+export const SITE_DESCRIPTION = "A state of mind journal. It's a place to write down your thoughts and feelings, and to reflect on your life. Currently on iOS only.";
